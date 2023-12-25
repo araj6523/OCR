e6c8b76802afde9eeed6a158866a0213219a6ec6
Ashish Raj <108401537+araj6523@users.noreply.github.com>
2023-12-25 17:09:11 +0530

    Deployment Changes

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def delete_data(identification_number):
     return result
 '''
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
