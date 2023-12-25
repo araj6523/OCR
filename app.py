c2799f498ddab82460f6b158b109db32194d623e
Ashish Raj <108401537+araj6523@users.noreply.github.com>
2023-12-25 21:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ def delete_data(identification_number):
     return result
 '''
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
